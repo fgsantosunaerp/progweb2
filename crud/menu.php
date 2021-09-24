663e682897c9c7e16b508331e883f223fbd22d3a
fabiano gonçalves <fgsantos@unaerp.br>
2021-09-24 20:33:26 -0300

    criação do crud

---
+++ crud/menu.php
@@ -0,0 +1,28 @@
+<div class="position-sticky pt-3">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+            <span data-feather="home"></span>   
+            Início
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="regioes/index.php">
+            <span data-feather="map-pin"></span>
+            Regiões
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="paises/index.php">
+            <span data-feather="globe"></span>
+            Países
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="locations/index.php">
+            <span data-feather="map"></span>
+            Localizações
+            </a>
+        </li>
+    </ul>
+</div>
