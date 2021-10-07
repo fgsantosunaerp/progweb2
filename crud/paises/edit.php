a80e27acf724869859bfe0242c93809811ca63d9
fabiano gonçalves <fgsantos@unaerp.br>
2021-10-07 19:12:33 -0300

    pdo

--- crud/paises/edit.php
+++ crud/paises/edit.php
@@ -8,17 +8,7 @@
     else {
 
         if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $country_id   = $_POST['country_id'];
-            $country_name = $_POST['country_name'];
-            $region_id    = $_POST['region_id'];
-
-            $sql = "insert into countries (country_id,country_name,region_id) values ('$country_id','$country_name','$region_id')";
-            if($conn->query($sql) === TRUE ){
-                echo "Novo registro inserido.";
-            }
-            else {
-                echo "Falha: ".$sql."\n".$conn->error;
-            }
+            
         }
         else {
             $id = $_GET['id'];
@@ -27,12 +17,11 @@
             $stmt->bind_param("s", $id);
             $stmt->execute();
             $result = $stmt->get_result();
-            print_r($result);
-            exit();
-            $country_id = $result->fetch_row()[0] ?? false;
-            $country_name = $result->fetch_row()[1] ?? false;
-            $region_id = $result->fetch_row()[2] ?? false;
+            
+            $dados = $result->fetch_row() ;
+            
         }
+
     }
 
 
@@ -40,19 +29,20 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Editando país</h1>
 </div>
-<form method="post" action="paises/new.php">
+<form method="post" action="paises/edit.php">
   <div class="mb-3">
     <label for="country_id" class="form-label">Id</label>
-    <input type="text" class="form-control" id="country_id" name="country_id" value="<?=$country_id;?>">
+    <input type="text" class="form-control" id="country_id" name="country_id" value="<?=$dados[0];?>">
   </div>
   <div class="mb-3">
     <label for="country_name" class="form-label">Nome</label>
-    <input type="text" class="form-control" id="country_name" name="country_name" value="<?=$country_name;?>">
+    <input type="text" class="form-control" id="country_name" name="country_name" value="<?=$dados[1];?>">
   </div>
   <div class="mb-3">
     <label for="region_id" class="form-label">Região</label>
-    <input type="text" class="form-control" id="region_id" name="region_id" value="<?=$region_id;?>">
+    <input type="text" class="form-control" id="region_id" name="region_id" value="<?=$dados[2];?>">
   </div>
 
-  <button type="submit" class="btn btn-primary">Inserir</button>
+  <button type="submit" class="btn btn-primary">Editar</button>
+  <button type="submit" class="btn btn-danger">Apagar</button>
 </form>
